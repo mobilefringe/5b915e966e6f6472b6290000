82bd83d4373b70a9231090f83446b1c1e932032b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-07 15:48:54 -0400

    Simcoe Town Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -13,7 +13,10 @@
                 <div class="main_container margin_30">
                     <div class="details_row">
                         <div class="details_col_3">
-                            <img class="store_details_image center-block" :src="currentStore.store_front_url_abs" :alt="currentStore.name + ' Logo'" />
+                            <div v-if="currentStore.no_logo" class="no_store_logo">
+                                
+                            </div>
+                            <img v-else class="store_details_image center-block" :src="currentStore.store_front_url_abs" :alt="currentStore.name + ' Logo'" />
                             <div v-if="currentStore.phone">
                                 <h3 class="inside_page_title">Phone</h3>
                                 <a class="store_details_phone" :href="'tel:' + currentStore.phone">{{ currentStore.phone }}</a>    
