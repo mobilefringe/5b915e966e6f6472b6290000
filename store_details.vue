bf3dea3b7b164f23740850f7963bf0b3fd751747
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-12 15:29:04 -0400

    Simcoe Town Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -44,7 +44,7 @@
                                 </li>
                             </ul>
                             <div class=" margin_30 store_details_desc" v-html="currentStore.rich_description"></div>
-                            <div v-if="this.currentStore.promotions">
+                            <div v-if="currentStore.promotions">
                                 <b-card no-body class="mb-1 inside_page_toggle">
                                     <b-card-header header-tag="header" class="p-1" role="tab">
                                         <b-btn block @click="togglePromos = !togglePromos" :aria-expanded="togglePromos ? 'true' : 'false'" aria-controls="togglePromotions">
@@ -73,7 +73,7 @@
                                     </b-collapse>
                                 </b-card>
                             </div>
-                            <div v-if="this.currentStore.jobs">
+                            <div v-if="currentStore.jobs">
                                 <b-card no-body class="mb-1 inside_page_toggle">
                                     <b-card-header header-tag="header" class="p-1" role="tab">
                                         <b-btn block @click="toggleJobs = !toggleJobs" :aria-expanded="toggleJobs ? 'true' : 'false'" aria-controls="toggleJobs">
