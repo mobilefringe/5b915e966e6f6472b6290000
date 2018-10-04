6e54dbc3127eac4ff222359a5b0b1beb3aa4100c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-04 17:14:19 -0400

    Simcoe Town Centre 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -151,7 +151,7 @@
                     var listTwo = [];
                     _.forEach( this.processedStores , function( value, key ) {
                         var starter = "A";
-                        var breaker = "J";
+                        var breaker = "I";
                         var store_initial = _.toUpper(value.name[0]);
                         if (store_initial.charCodeAt(0) <= breaker.charCodeAt(0) && store_initial.charCodeAt(0) >= starter.charCodeAt(0)){
                             listOne.push(value);
