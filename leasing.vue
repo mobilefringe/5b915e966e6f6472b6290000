9e00f129f57f9f3dd89bd036a1e09a49e5cc014e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-23 15:40:57 -0400

    Simcoe Town Centre 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -82,7 +82,7 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Map Banner').images;
+                    var temp_repo = this.findRepoByName('Leasing Banner').images;
                     if(temp_repo != null) {
                         this.pageBanner = temp_repo[0];
                     } else {
