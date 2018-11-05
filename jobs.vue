b916e7f639be4bd280f18c0a113b813679f822d8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-05 14:48:56 -0500

    Simcoe Town Centre 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -76,6 +76,7 @@ define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue
                     dataLoaded: false,
                     pageBanner: null,
                     toggleJobs: false,
+                    sideBanner:null
                 }
             },
             created() {
@@ -88,7 +89,7 @@ define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
                         }
                     }
-                    var temp_repo1 = this.findRepoByName('Events Side Banner').images;
+                    var temp_repo1 = this.findRepoByName('Jobs Side Banner').images;
                     if(temp_repo1 != null) {
                         this.sideBanner = temp_repo1[0];
                     } else {
