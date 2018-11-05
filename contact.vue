047ab10a5a3176e9ea24b844448a066f56f85458
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-05 14:43:04 -0500

    Simcoe Town Centre 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -99,6 +99,15 @@
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
                         }
                     }
+                    var temp_repo = this.findRepoByName('Contact Side Banner').images;
+                    if(temp_repo != null) {
+                        this.pageBanner = temp_repo[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
+                        }
+                    }
+                    sideBanner
                     this.currentPage = response[0].data;
                     this.dataLoaded = true;
                 });
