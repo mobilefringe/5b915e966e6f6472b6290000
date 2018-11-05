88fd91ee2b8d5f6da06db71b96564c9cc5b21f54
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-05 14:57:46 -0500

    Simcoe Town Centre 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -11,7 +11,7 @@
                 <div class="main_container mobile_padding margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/jpeg/1536266625000/sidebanner1-compressor.jpg" alt="" />    
+                            <img class="img_max" :src="sideBanner.image_url" alt="" />    
                         </div>
                         <div class="details_col_9">
                             <!-- PROMOTIONS -->
