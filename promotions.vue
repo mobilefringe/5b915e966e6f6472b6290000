1a8303a53d5fd71655c0721696ee14510c950b3b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:14:38 -0500

    Simcoe Town Centre 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -94,7 +94,7 @@
                         }
                     }
                     var temp_repo1 = this.findRepoByName('Promotions Side Banner');
-                    if(temp_repo1 != null) {
+                    if(temp_repo1 && temp_repo1.images) {
                         this.sideBanner = temp_repo1.images[0];
                     } else {
                         this.sideBanner = {
