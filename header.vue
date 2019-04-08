c289f5a2af10d1396d34567fb23a06def4ccf628
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-04-08 09:45:52 -0400

    Simcoe Town Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -103,10 +103,14 @@
         </section>
     </header>
 </template>
-
+<style>
+    p.card-text {
+        display: block;
+    }
+</style>
 <script>
     define(["Vue", "vuex", "vue!today_hours.vue", "bootstrap-vue", "json!site.json", "vue!search-component"], function (Vue, Vuex, TodayHoursComponent, BootstrapVue, site, SearchComponent) {
-        Vue.use(BootstrapVue);
+        // Vue.use(BootstrapVue);
         return Vue.component("header-component", {
             template: template, // the variable template will be injected,
             data: function () {
